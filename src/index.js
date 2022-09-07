bf7a4e9edd567877589f65bfa48062e90bd8cd5e
Fatih Unal <fatihsultan069@gmail.com>
2022-09-07 15:37:24 +0300

    Example portfolio Website has finished

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 import App from "./App"
 import { ThemeProvider } from "./context"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
+
+ReactDOM.render(
 	<React.StrictMode>
 		<ThemeProvider>
 			<App />
 		</ThemeProvider>
-	</React.StrictMode>
+	</React.StrictMode>,
+	document.getElementById("root")
 )
